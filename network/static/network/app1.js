0221673b918b6221c996cf46b8fd8ac0794449cc
ErChaitanya <80812069+ErChaitanya@users.noreply.github.com>
2022-05-24 13:43:34 +0530

    second commit

---
+++ network/static/network/app1.js
@@ -0,0 +1,29 @@
+document
+  .getElementById("open-popup-btn")
+  .addEventListener("click", function () {
+    document.getElementsByClassName("popup")[0].classList.add("active");
+  });
+
+document
+  .getElementById("open-popup-btn1")
+  .addEventListener("click", function () {
+    document.getElementsByClassName("popup")[0].classList.add("active");
+  });
+
+document
+  .getElementById("open-popup-btn2")
+  .addEventListener("click", function () {
+    document.getElementsByClassName("popup")[0].classList.add("active");
+  });
+
+document
+  .getElementById("open-popup-btn3")
+  .addEventListener("click", function () {
+    document.getElementsByClassName("popup")[0].classList.add("active");
+  });
+
+document
+  .getElementById("dismiss-popup-btn")
+  .addEventListener("click", function () {
+    document.getElementsByClassName("popup")[0].classList.remove("active");
+  });
